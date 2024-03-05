9e11b465e9345e6d62bd0d72133f646fcfbe99bf
Sara Oliveira <sara.desider@gmail.com>
2024-03-05 15:55:22 -0300

    add mask em #telefone

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
-    $('#telefone').masck('(00)00000-0000', {placeholder: '(01)23456-7890'});
+    $('#telefone').mask('(00)00000-0000', {placeholder: '(01)23456-7890'});
 
 })
